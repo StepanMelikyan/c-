6a6d6172e6cc6479c95beee6fcd4d737b2181e3a
StepanMelikyan <98053309+StepanMelikyan@users.noreply.github.com>
2022-08-08 21:57:09 +0300

    Add files via upload
    task2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int A = 0;
+int B = 0;
+
+Console.WriteLine("Введите число А");
+A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+B = Convert.ToInt32(Console.ReadLine());
+
+if (A > B)
+{
+    Console.WriteLine(" максимальное число = " +A );
+}
+if (A < B)
+{
+    Console.WriteLine("B - максимальное число = " +B );
+}
+if (A == B)
+{
+    Console.WriteLine("числа равны друг другу");
+}
